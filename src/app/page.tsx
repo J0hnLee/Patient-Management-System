51fad28845574841000519aaca414c7ae87ffd3a
john791016 <john791016@gmail.com>
2024-08-28 01:11:55 +0800

    feat:setup and write README.md

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="hello">
+      <div className="33">
+        <p>hello</p>
+      </div>
+    </main>
+  );
+}
