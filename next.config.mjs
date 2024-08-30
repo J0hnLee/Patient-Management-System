1df97caea6f739e10418a8e2a5f444f90e25ca20
john791016 <john791016@gmail.com>
2024-08-31 00:49:54 +0800

    feat:homePage v1.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.cache = false; // 禁用 Webpack 缓存
+    return config;
+  }
+};
 
 export default nextConfig;
