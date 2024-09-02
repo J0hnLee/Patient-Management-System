6d1fa194e2bfcc5a10409a7c8462ea712d569ae8
john791016 <john791016@gmail.com>
2024-09-03 00:32:38 +0800

    feat:finish homepage

--- src/components/forms/PatientForm.tsx
+++ src/components/forms/PatientForm.tsx
@@ -1,7 +1,93 @@
-import React from "react";
+"use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { set, useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form } from "@/components/ui/form";
+import CustomFormField from "./CustomFormField";
+
+import SubmitButton from "@/components/submitButton";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+export enum FormFieldType {
+  INPUT = "input",
+  CHECKBOX = "checkbox",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneInput",
+  DATE_PICKER = "datePicker",
+  SELECT = "select",
+  SKELETON = "skeleton"
+}
 const PatientForm = () => {
-  return <div>PatientForm</div>;
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      username: "",
+      email: "",
+      phone: ""
+    }
+  });
+
+  // 2. Define a submit handler.
+  async function onSubmit({
+    username,
+    email,
+    phone
+  }: z.infer<typeof UserFormValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    setIsLoading(true);
+    try {
+      const userData = {
+        username,
+        email,
+        phone
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi There!</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="username"
+          label="全名"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="leadwayrx@gmail.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="電話"
+          placeholder=""
+        />
+
+        <SubmitButton isLoading={isLoading}>提交</SubmitButton>
+      </form>
+    </Form>
+  );
 };
 
 export default PatientForm;
