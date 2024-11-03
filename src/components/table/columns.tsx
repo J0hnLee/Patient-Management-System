092e9d0b04bc34a609f69bafaca7b05fdd8a87e8
john791016 <john791016@gmail.com>
2024-11-04 00:42:07 +0800

    feat:add admin page.

---
+++ src/components/table/columns.tsx
@@ -0,0 +1,90 @@
+"use client";
+import { MoreHorizontal } from "lucide-react";
+import { formatDateTime } from "@/lib/utils";
+import { Doctors } from "@/constants";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ColumnDef } from "@tanstack/react-table";
+import StatusBadage from "../StatusBadage";
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    header: "id",
+    cell: ({ row }) => {
+      console.log("🚀 ~ row:", row);
+      return <p>{row.index + 1}</p>;
+    },
+  },
+  {
+    accessorKey: "patient",
+    header: "病患",
+    cell: ({ row }) => (
+      <p className="text-14-medium">{row.original.patient.name}</p>
+    ),
+  },
+  {
+    accessorKey: "status",
+    header: "狀態",
+    cell: ({ row }) => <StatusBadage status={row.original.status} />,
+  },
+
+  {
+    accessorKey: "schedule",
+    header: "Appointment",
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return (
+        <p className="text-14-regular min-w-[100px]">
+          {formatDateTime(appointment.schedule).dateTime}
+        </p>
+      );
+    },
+  },
+  {
+    accessorKey: "primaryPhysician",
+    header: "Doctor",
+    cell: ({ row }) => {
+      const appointment = row.original;
+
+      const doctor = Doctors.find(
+        (doctor) => doctor.name === appointment.primaryPhysician
+      );
+
+      return (
+        <div className="flex items-center gap-2">
+          <Image
+            src={doctor?.image!}
+            alt="doctor"
+            width={100}
+            height={100}
+            className="size-8"
+          />
+          <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+        </div>
+      );
+    },
+  },
+  {
+    id: "actions",
+    header: () => <div className="pl-4">Actions</div>,
+    cell: ({ row }) => {
+      return <div className="flex gap-1">AppointmentModal</div>;
+    },
+  },
+];
